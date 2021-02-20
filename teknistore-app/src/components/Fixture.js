038bbd8934d4a53ad51cfe17f190f09f01befe4e
FlorianClanet <florianclanet@gmail.com>
2021-02-20 09:43:49 +0100

    feat|add grid style

--- teknistore-app/src/components/Fixture.js
+++ teknistore-app/src/components/Fixture.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
-const Fixture = ({ name, description }) => {
+const Fixture = (props) => {
     return (
-        <div className="w-full flex flex-col justify-center items-center">
-            <div className='max-w-lg bg-white shadow-md rounded-lg overflow-hidden mx-auto'>
-                <div className="py-4 px-8 mt-3">
-                    <div className="flex flex-col mb-8">
-                    <h2 className="text-gray-700 font-semibold text-2xl tracking-wide mb-2">{name}</h2>
-                    <p>{description? description : "description"}</p>
+        <>
+            <div className="w-full flex flex-col justify-center items-center">
+                <div className='w-full bg-white shadow-md rounded-lg overflow-hidden'>
+                    <div className="py-8 px-8 mt-3">
+                        <div className="flex flex-col mb-8">
+                        <h2 className="text-gray-700 font-semibold text-2xl tracking-wide mb-2">{props.name}</h2>
+                        <p>{props.description? props.description : "description"}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- teknistore-app/src/components/FixtureList.js
+++ teknistore-app/src/components/FixtureList.js
@@ -3,9 +3,20 @@ import Fixture from './Fixture'
 
 const FixtureList = ({ data }) => {
     return (
-        <div className="flex flex-wrap justify-center">
-            {data.map(fixture => <Fixture name={fixture.name} description={fixture.description} key={fixture.name} />)}
-        </div>
+        <>
+            
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-8">
+                {data.map(fixture => {
+                    return (
+                        <>
+                            <div>
+                                <Fixture name={fixture.name} description={fixture.description} key={fixture.name} />
+                            </div>
+                        </>
+                    ); 
+                    })}
+            </div>
+      </>
     );
 }
 
