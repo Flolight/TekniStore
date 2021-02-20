53feb2a3aa2b01a43c87db055969c2d8e2efa8b8
FlorianClanet <florianclanet@gmail.com>
2021-02-20 08:56:02 +0100

    fix|change "a" to "span" into Link to avoid syntax errors

--- teknistore-app/src/components/NavBar.js
+++ teknistore-app/src/components/NavBar.js
@@ -9,9 +9,9 @@ const NavBar = () => {
                     <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                         <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
                             <Link to="/">
-                                <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#">
+                                <span className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#">
                                     TekniStore
-                                </a>
+                                </span>
                                 <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
                                     <span className="block relative w-6 h-px rounded-sm bg-white"></span>
                                     <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
@@ -23,30 +23,30 @@ const NavBar = () => {
                         <ul className="flex flex-col lg:flex-row list-none ml-auto">
                             <li className="nav-item">
                                 <Link to="/dashboard">
-                                    <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                                    <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
                                         Dashboard
-                                    </a>
+                                    </span>
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/stock">
-                                    <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                                    <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
                                         Stock
-                                    </a>
+                                    </span>
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/gigs">
-                                    <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                                    <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
                                         Gigs
-                                    </a>
+                                    </span>
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/profile">
-                                    <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                                    <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
                                         Profile
-                                    </a>
+                                    </span>
                                 </Link>
                             </li>
                         </ul>
